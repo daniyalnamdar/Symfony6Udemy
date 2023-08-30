57acbd897b847f7f3cff9ee48c4e187e80aa1ebd
Daniyal Namdar <daniyal.namdar@bktomorrow.com>
2023-08-30 15:44:51 +0300

    end of session 6

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\MicroPostRepository;
+use App\Repository\UserProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use \Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,5 +46,47 @@ class HelloController extends AbstractController
         );
 
     }
+
+    #[Route('/', name: 'user_profile', methods: ['GET'])]
+    public function userProfile(EntityManagerInterface $om, MicroPostRepository $repository): Response
+    {
+//        $post = $repository->find(10);
+//        dd($post);
+//        $post = new MicroPost();
+//        $post->setTitle('Hello there');
+//        $post->setText('Text for Hello');
+//        $post->setCreated(new \DateTime());
+//
+//        $comment = new Comment();
+//        $comment->setText('Hello');
+//
+//        $post->addComment($comment);
+//        $om->persist($post);
+//        $om->flush();
+
+//        $user = new User();
+//        $user->setEmail('emailww@gmail.com');
+//        $user->setPassword('123456');
+//
+//        $profile = new UserProfile();
+//        $profile->setUser($user);
+//        $om->persist($profile);
+//
+//
+//        $om->flush();
+
+//        $profile = $repository->find(1);
+//        $om->remove($profile);
+//        $om->flush();
+
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => 's'
+            ]
+        );
+
+    }
+
 
 }
